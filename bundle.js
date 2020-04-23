8b6d176b8959d1d5b09d8b6eecb8c64f2101ec68
Guilherme Morone Araujo <guimorone@gmail.com>
2020-04-22 22:53:26 -0300

    Valores padrões em funções

--- bundle.js
+++ bundle.js
@@ -1,58 +1,14 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-/* //usando classes
-
-class List {
-  //tipo __init__ em python
-  constructor() {
-    this.data = [];
-  }
-
-  add(id) {
-    this.data.push(id);
-    console.log(this.data);
-  }
+//valores padrão
+//serve para arrow functions também
+//VALORES PADRÃO SEMPRE TEM QUE VIM DEPOIS DOS VALORES QUE SÃO OBRIGATÓRIOS
+//por exemplo: function soma(a = 3, b = 6), function soma(a, b = 6)
+//já function soma(a = 3, b) tá errado
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 }
 
-class TodoList extends List{
-    constructor() {
-        super(); //super chama o constructor de List (classe pai da herança)
-        this.usuario = "Guilherme";
-    }
-
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
-}
-
-var teste = new TodoList();
-
-document.getElementById("novotodo").onclick = () => {
-    teste.add("Novo todo");
-}
-
-teste.mostraUsuario(); */
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    //static não le oq tem no resto da classe
-    //eh um método meio independente
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2));
+console.log(soma(2));
--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-/* //usando classes
+//valores padrão
+//serve para arrow functions também
+//VALORES PADRÃO SEMPRE TEM QUE VIM DEPOIS DOS VALORES QUE SÃO OBRIGATÓRIOS
+//por exemplo: function soma(a = 3, b = 6), function soma(a, b = 6)
+//já function soma(a = 3, b) tá errado
 
-class List {
-  //tipo __init__ em python
-  constructor() {
-    this.data = [];
-  }
-
-  add(id) {
-    this.data.push(id);
-    console.log(this.data);
-  }
+function soma(a = 3, b = 6) {
+    return a + b;
 }
 
-class TodoList extends List{
-    constructor() {
-        super(); //super chama o constructor de List (classe pai da herança)
-        this.usuario = "Guilherme";
-    }
-
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
-}
-
-var teste = new TodoList();
-
-document.getElementById("novotodo").onclick = () => {
-    teste.add("Novo todo");
-}
-
-teste.mostraUsuario(); */
-
-class Matematica {
-    //static não le oq tem no resto da classe
-    //eh um método meio independente
-    static soma(a, b) {
-        return a + b
-    }
-}
-
-console.log(Matematica.soma(1, 2));
+console.log(soma(2));
