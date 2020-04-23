2ee6cb35366f2e4f122f58ca16a274a7f9ad6ffc
Guilherme Morone Araujo <guimorone@gmail.com>
2020-04-22 23:25:25 -0300

    Webpack configurado

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-//Template literals
+import {soma} from "./funcoes";
 
-const nome = "Guilherme";
-const idade = 19;
-
-//console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
-//isso dai de cima eh a mesma coisa disso daki de baixo
-
-console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
-//sim isso eh um acento (crase `) e não aspas simples
-
-//object short syntax
-
-const usuario = {
-    nome, //nome: nome,
-    idade, //idade: idade,
-    país: "Brasil"
-};
-
-console.log(usuario);
+console.log(soma(1, 2));
