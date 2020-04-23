f3cd8c0ec41a752f813e61193806f3b7b831e5da
Guilherme Morone Araujo <guimorone@gmail.com>
2020-04-23 02:16:49 -0300

    Aplicação com ES6+

--- src/main.js
+++ src/main.js
@@ -1,16 +1,91 @@
-//Usando async/await com axios
-import axios from "axios";
+import api from "./api";
 
-class Api {
-    static async getUserInfo(username) {
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch(err) {
-            console.warn("Erro na API.");
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById("repo-form");
+        this.inputEl = document.querySelector("input[name=repository]");
+        this.listEl = document.getElementById("repo-list");
+
+        this.registerHandlers();
+    }
+
+    //registrar os eventos
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if(loading) {
+            let loadingEL = document.createElement("span");
+            loadingEL.appendChild(document.createTextNode("Carregando"));
+            loadingEL.setAttribute("id", "loading");
+
+            this.formEl.appendChild(loadingEL);
+        } else{
+            document.getElementById("loading").remove();
         }
+    }
+
+    async addRepository(event) {
+        event.preventDefault(); //não recarregar a página
+
+        const repoInput = this.inputEl.value;
+
+        if(!repoInput.length) return; //parar a função
+
+        this.setLoading(); //aparecer uma mensagem de q tá carregando as informações
+        
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+
+            this.inputEl.value = "";
+
+            this.render();
+        } catch(err) {
+            alert("O repositório não existe!");
+        }
+        
+        this.setLoading(false);
+    }
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement("img");
+            imgEl.setAttribute("src", repo.avatar_url);
+
+            let titleEl = document.createElement("strong");
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement("p");
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement("a");
+            linkEl.setAttribute("target", "_blank"); //abre uma nova aba já com o botão esquerdo do mouse
+            linkEl.setAttribute("href", repo.html_url);
+            linkEl.appendChild(document.createTextNode("Acessar"));
+
+            let listItemEl = document.createElement("li");
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-Api.getUserInfo("adasdasdsadas"); //vai dar erro, mas eh só pra teste
-Api.getUserInfo("guimorone");
+new App();
